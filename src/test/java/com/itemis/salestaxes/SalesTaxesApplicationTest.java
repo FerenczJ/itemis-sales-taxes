0f15ba06f0c0c8e8d8c9a762a8020a139e95acca
mail.ferencz <mail.ferencz@gmail.com>
2024-01-27 14:53:49 +0100

    feature/ITEMIS01 Initial project structure.

---
+++ src/test/java/com/itemis/salestaxes/SalesTaxesApplicationTest.java
@@ -0,0 +1,16 @@
+package com.itemis.salestaxes;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+
+class SalesTaxesApplicationTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+}
